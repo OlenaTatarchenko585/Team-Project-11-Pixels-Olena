c094b6271a2a59d90cc2ffb624a7e482ec680634
Olena <olenatatarch585@gmail.com>
2026-01-02 14:42:41 +0200

    Set base path for project in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/Team-Project-11-Pixels-Olena/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
