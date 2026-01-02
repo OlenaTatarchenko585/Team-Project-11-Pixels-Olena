91f268c9e452a82a2e633b4b758486792177ea62
Olena <olenatatarch585@gmail.com>
2026-01-02 20:10:59 +0200

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: './',
+    base: '/Team-Project-11-Pixels/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
